ad809ccca938f39939e872fe55c331487373bd47
pooja sethiya <poojasethiya24@gmail.com>
2020-08-17 22:05:24 +0530

    orf length and codon optimaltiy script added

---
+++ R/optimal_codon_distribution.R
@@ -0,0 +1,107 @@
+#' @title optimal_codon_distribution
+#' @description Codon distribution based on pre-defined optimal codons for yeast
+#'   species (Pechmann, 2013)
+#' @param optimal_codon_table Codon table, containing pre-defined optimal and
+#'   Non-optimal codons
+#' @param genelist_stabilityInfo tibble, of genes with stability information.
+#'   First_column: gene_id, second_column: stability ie. whether the gene is
+#'   stable or unstable, third_column: condition ie. stressed or unstressed
+#' @param ref_sequence_file path or connection of coding nucleotides sequences
+#'   fasta file. The number of sequences should be equal to number of genes in
+#'   genelist_stabilityInfo tibble. Sequences can be subsetted from all genes
+#'   sequences using fastaR::fa_some_records() function.
+#' @return a tibble of optimal codon percentage for each gene, boxplot of codon
+#'   distribution in different class or stability of genes.
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  codon_table <- system.file("extdata", "codonOptimalityTable_Yeasts.txt", package = "FungalTranscriptStability")
+#'  org_codon_table <- readr::read_tsv(codon_table) %>% dplyr::select(Codon, Cgla)
+#'  ref_sequence_file <- system.file("extdata", "CgROS_stability.fa", package = "FungalTranscriptStability")
+#'  }
+#' }
+#' @rdname optimal_codon_distribution
+#' @export
+#' @importFrom broom tidy
+#' @importFrom dplyr mutate left_join group_by filter select
+#' @importFrom purrr map map2 map_dbl
+#' @importFrom seqinr read.fasta uco getSequence
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr gather unnest nest spread
+#' @importFrom ggplot2 ggplot aes facet_wrap geom_boxplot scale_fill_manual
+#'   scale_y_log10 theme_bw labs scale_x_discrete theme element_blank
+#'   element_text element_rect
+optimal_codon_distribution <- function(optimal_codon_table,genelist_stabilityInfo, ref_sequence_file){
+
+  colnames(genelist_stabilityInfo) <- c("gene_id", "stability", "condition") # rename columns of stability info table
+
+  colnames(optimal_codon_table) <- c("codon", "organism") # rename columns of codon table
+
+  seq_dat <- broom::tidy(ref_sequence_file)
+
+  codon_calci <- seq_dat %>%
+    dplyr::mutate(cond_freq=purrr::map(x, function(ii){
+      seqinr::read.fasta(ii,as.string = TRUE,forceDNAtolower = FALSE) %>% # read input fasta file
+        purrr::map(~seqinr::uco(seqinr::getSequence(.x, as.string = FALSE),frame = 0,index="eff")) %>% # separate the sequences as codons and calculate freq of each codon
+        do.call("cbind",.)  %>%
+        tibble::as_tibble(rownames = "codon") %>%
+        dplyr::mutate(codon=toupper(codon)) %>% # convert lower case to upper to match codon table
+        dplyr::left_join(optimal_codon_table, by = c("codon"))  %>% # combine codon table with codon freq
+        tidyr::gather("gene_id","freq", -codon, -organism) %>%
+        dplyr::group_by(gene_id) %>%
+        dplyr::mutate(total = sum(freq)) %>% # calculate total N and O frequencies
+        dplyr::filter(organism %in% "O") %>%
+        dplyr::mutate(optimal=sum(freq), percent=100*(optimal/total)) })) %>% # calculate percentage of optimal codons
+    dplyr::select(-c(x)) %>%
+    tidyr::unnest(cols = c(cond_freq)) %>%
+    dplyr::select(c(gene_id, total, optimal, percent)) %>%
+    unique()
+
+  combine_stability <- dplyr::left_join(codon_calci, genelist_stabilityInfo)
+
+  if(any(is.na(combine_stability))){
+
+    print("no. of genes in stabilityInfo", nrow(genelist_stabilityInfo))
+    print("no. of sequence", nrow(codon_calci))
+    warning("sequence file and stability does not match!")
+
+  }
+
+  gp <- combine_stability %>%
+    ggplot2::ggplot(ggplot2::aes(stability, percent, fill=stability)) +
+    ggplot2::facet_wrap(~condition)+
+    ggplot2::geom_boxplot(notch = TRUE, notchwidth = 0.4, alpha=0.8, width=0.5) +
+    ggplot2::scale_fill_manual(values=c("#FC4E07", "#00AFBB" ))+
+    ggplot2::scale_y_log10()+
+    ggplot2::theme_bw()+
+    ggplot2::labs(x="", y="Optimal codons (in %)")+
+    ggplot2::scale_x_discrete(position="top")+
+    ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                   axis.ticks.x = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(color="black",size=12),
+                   panel.border = ggplot2::element_rect(color="black", size=1),
+                   axis.title.y = ggplot2::element_text(color="black",size=12, face="bold"),
+                   strip.text   = ggplot2::element_text(color="black",size=12,face="bold"),
+                   panel.grid   = ggplot2::element_blank(),
+                   strip.background  = ggplot2::element_rect(fill = "white", color="black", size=1),
+                   legend.position  = "bottom"
+    )
+
+  print(gp)
+
+  # compute p-value
+  t_test <- combine_stability %>%
+    dplyr::select(c(stability, condition, percent)) %>%
+    dplyr::group_by(stability, condition) %>%
+    tidyr::nest() %>%
+    tidyr::spread(key = stability, value = data) %>%
+    dplyr::mutate(
+      t_test = purrr::map2(.[[2]], .[[3]], ~ t.test(.x$percent, .y$percent) %>% broom::tidy())) %>%
+    dplyr::mutate(pval = purrr::map_dbl(t_test , ~ .$`p.value`)) %>%
+    dplyr::select(condition, pval)
+
+  print(t_test)
+
+  return(combine_stability)
+
+}
---
+++ R/orf_len.R
@@ -0,0 +1,80 @@
+#' @title orf_len_distribution
+#' @description displays distribution of orf lengths for give stable and
+#'   unstable genes.
+#' @param gff path or connection of gff file
+#' @param genelist_stabilityInfo tibble, of genes with stability information.
+#'   First_column: gene_id, second_column: stability ie. whether the gene is
+#'   stable or unstable, third_column: condition ie. stressed or unstressed
+#' @return pvalue of the orf lengths, boxplot of orf length distribution
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'   gff_file <- "/Users/Pooja/Documents/PhD/thesis_figures/Chapter3/C_glabrata_CBS138_version_s02-m07-r38_features.gff"
+#'  }
+#' }
+#' @rdname orf_len_distribution
+#' @export
+#' @importFrom GenomicFeatures makeTxDbFromGFF transcriptLengths
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr select left_join mutate group_by summarize
+#' @importFrom forcats as_factor
+#' @importFrom ggplot2 ggplot aes facet_wrap geom_boxplot scale_fill_manual
+#'   scale_y_log10 theme_bw labs scale_x_discrete theme element_blank
+#'   element_text element_rect
+#' @importFrom tidyr nest spread
+#' @importFrom purrr map2 map_dbl
+#' @importFrom broom tidy
+orf_len_distribution <- function(gff, genelist_stabilityInfo){
+
+  colnames(genelist_stabilityInfo) <- c("gene_id", "stability", "condition")
+
+  # load gene features
+  gff <- GenomicFeatures::makeTxDbFromGFF(gff, metadata = T, format="gff")
+  cds_len <- GenomicFeatures::transcriptLengths(gff, with.cds_len = T) %>%
+                tibble::as_tibble() %>% dplyr::select(c(gene_id,cds_len))
+
+  # match gene length to the input genes
+    gene_width <- dplyr::left_join(genelist_stabilityInfo, cds_len) %>%
+                  dplyr::mutate(stability=forcats::as_factor(stability))
+
+  # calculate median length
+  median = gene_width %>%
+            dplyr::group_by(stability) %>%
+            dplyr::summarize(med = median(cds_len))
+
+  print(median)
+
+  # plot boxplot
+  gg <- gene_width %>%
+    ggplot2::ggplot(ggplot2::aes(stability, cds_len, fill=stability))+
+    ggplot2::facet_wrap(~condition)+
+    ggplot2::geom_boxplot(notch = TRUE, notchwidth = 0.4, alpha=0.8, width=0.5) +
+    ggplot2::scale_fill_manual(values=c("#FC4E07", "#00AFBB" ))+
+    ggplot2::scale_y_log10()+
+    ggplot2::theme_bw()+
+    ggplot2::labs(x="", y="ORF lenght (in bp)")+
+    ggplot2::scale_x_discrete(position="top")+
+    ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+          axis.ticks.x = ggplot2::element_blank(),
+          axis.text.y = ggplot2::element_text(color="black",size=12),
+          panel.border = ggplot2::element_rect(color="black", size=1),
+          axis.title.y = ggplot2::element_text(color="black",size=12, face="bold"),
+          strip.text   = ggplot2::element_text(color="black",size=12,face="bold"),
+          panel.grid   = ggplot2::element_blank(),
+          strip.background  = ggplot2::element_rect(fill = "white", color="black", size=1),
+          legend.position  = "bottom"
+    )
+  print(gg)
+
+  t_test <- gene_width %>%
+    dplyr::group_by(stability, condition) %>%
+    tidyr::nest() %>%
+    tidyr::spread(key = stability, value = data) %>%
+    dplyr::mutate(
+      t_test = purrr::map2(.[[2]], .[[3]], ~ t.test(.x$cds_len, .y$cds_len) %>% broom::tidy())) %>%
+    dplyr::mutate(pval = purrr::map_dbl(t_test , ~ .$`p.value`)) %>%
+    dplyr::select(condition, pval)
+
+  return(t_test)
+}
+
